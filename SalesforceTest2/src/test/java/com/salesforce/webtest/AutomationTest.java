446730f49832edcea5706bb0c38214e0a5d600bc
rsalavarria <royevies@gmail.com>
2020-03-10 13:05:24 -0500

    Esperas explicitas

--- SalesforceTest2/src/test/java/com/salesforce/webtest/AutomationTest.java
+++ SalesforceTest2/src/test/java/com/salesforce/webtest/AutomationTest.java
@@ -10,7 +10,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutomationTest {
 	
@@ -23,10 +25,10 @@ public class AutomationTest {
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://test1dom--sit.my.salesforce.com/secur/frontdoor.jsp?sid=00Dc0000003lOc8!ARsAQOIjf63jURW2O3jPPhnS_8n6eQgkyek9yD22A9yK3CzgWGjypSRPWiWVUSavG3MWauwhkjCDhETXfhnY7uE3ayjjZKTl");
+		driver.get("https://test1dom--sit.my.salesforce.com/secur/frontdoor.jsp?sid=00Dc0000003lOc8!ARsAQKMaCa1WmbO7z7tm2pJj64Uzf.kuDoXPogLaqzE.aGYT0_JokvR4QOEEHyMb_Nt7kR67sxuUMvz0CUPDm5ORrg7jp8jy");
 		//driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		
 		Thread.sleep(20000);
 		
@@ -41,14 +43,25 @@ public class AutomationTest {
 		
 		driver.findElement(By.xpath("//input[@id='seibelUser']")).sendKeys("19991993");
 
-		Thread.sleep(4000);
+		//Thread.sleep(4000);
 		//Botón: Buscar Cliente
+		
+		WebElement res = new WebDriverWait(driver, 40)
+		        .until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='IP_validClient']")));
 
-		driver.findElement(By.xpath("//div[@id='IP_validClient']")).click();
-		Thread.sleep(4000);
+		driver.findElement(By.id(res.getAttribute("id"))).click();
+		
+		WebElement res1 = new WebDriverWait(driver, 40)
+		        .until(ExpectedConditions.elementToBeClickable(By.id("RadioUpdateDatosSeibel")));
+
+		//driver.findElement(By.xpath("//div[@id='IP_validClient']")).click();
+		//Thread.sleep(4000);
 
 		// Cambiar indice a 1 para seleccionar opcion "si"
 		RoynerClass.actualizarCliente(driver, 0);
+		
+		WebElement res2 = new WebDriverWait(driver, 40)
+		        .until(ExpectedConditions.elementToBeClickable(By.id("block_0")));
 
 		//------------------------------------PLANES-----------------------
 		/*Aca se puede comentar o descomentar según sea necesario probar*/
